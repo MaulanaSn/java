aa1ca3adad2fcb12e396c18e007517fe595ce605
MaulanaSn <111726975+MaulanaSn@users.noreply.github.com>
2022-08-22 14:45:37 +0700

    Add files via upload

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+     public static void main (String [] args) 
+     {
+           System.out.println("Haloo semua...");
+           System.out.println("Selamat Belajar Java");
+          
+
+
+     }
+}
